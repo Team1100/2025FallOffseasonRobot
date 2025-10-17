402c3d3152060d8078164cfae2e9a476657e9e71
MegaCraftCat <MegaCraftCat@gmail.com>
2025-10-17 18:02:52 -0400

    Pivoty Thing attempt 1

--- src/main/java/frc/robot/subsystems/PivotyThing.java
+++ src/main/java/frc/robot/subsystems/PivotyThing.java
@@ -4,14 +4,49 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import frc.robot.Constants;
+import frc.robot.RobotMap;
+import frc.robot.testingdashboard.SubsystemBase;
+import frc.robot.testingdashboard.TDNumber;
 
 public class PivotyThing extends SubsystemBase {
-  /** Creates a new PivotyThing. */
-  public PivotyThing() {}
+
+  private static PivotyThing m_PivotyThing = null; 
+  
+  SparkMax m_FLeftSparkMax;
+
+  SparkMaxConfig m_SparkMaxConfig;
+
+  private PivotyThing() {
+    super("PivotyThing");
+
+    if (-1.F_ENABLED) {
+      m_FLeftSparkMax = new SparkMax(-1.F_LEFTMOTOR, MotorType.kBrushless);
+
+      m_SparkMaxConfig = new SparkMaxConfig();
+      
+      //m_FLeftSparkMax.configure(m_SparkMaxConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters); ?
+
+      //m_FunnelCurrentOutput = new TDNumber(this, "Funnel", "Motor Current"); ?
+    }
+  }
+
+  public static PivotyThing getInstance(){
+    if (m_PivotyThing == null){
+      m_PivotyThing = new PivotyThing();
+    }
+    return m_PivotyThing;
+  }
+  
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
+
+  
 }
