bac287aecd6ce4d7d866db4c7a7ffd3ed2103ffd
MegaCraftCat <MegaCraftCat@gmail.com>
2025-10-18 16:54:26 -0400

    Drive Rate Limiting :3

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -4,22 +4,28 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
+import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.util.WPIUtilJNI;
 import edu.wpi.first.wpilibj.ADIS16470_IMU;
 import edu.wpi.first.wpilibj.ADIS16470_IMU.IMUAxis;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import frc.robot.Constants;
 import frc.robot.RobotMap;
 import frc.robot.testingdashboard.SubsystemBase;
+import frc.robot.testingdashboard.TDNumber;
 import frc.robot.testingdashboard.TDSendable;
 
 public class Drive extends SubsystemBase {
@@ -37,9 +43,59 @@ public class Drive extends SubsystemBase {
     RobotMap.D_BACK_RIGHT_DRIVE_MOTOR_CAN_ID, RobotMap.D_BACK_RIGHT_TURN_MOTOR_CAN_ID, Constants.DriveModuleConstants.kBackRightChassisAngularOffset);
 
 
+    
+
+
   private final ADIS16470_IMU m_Gyro = new ADIS16470_IMU();  
-  private SwerveDrivePoseEstimator m_DrivePoseEstimator;
   private final Field2d m_Field;
+
+  //these arent used but they seem good to have
+  private double m_currentRotation = 0.0;
+  private double m_currentTranslationDir = 0.0;
+  private double m_currentTranslationMag = 0.0;
+
+  private double m_prevTime = WPIUtilJNI.now() * 1e-6;
+  private ChassisSpeeds m_lastSpeeds = new ChassisSpeeds();
+
+  //SwerveCrivePoseEstimator class for tracking robot pose
+  SwerveDrivePoseEstimator m_DrivePoseEstimator;
+
+  //testing dashboard instances
+  TDNumber TDxSpeedCommanded;
+  TDNumber TDySpeedCommanded;
+  TDNumber TDrotSpeedCommanded;
+
+  TDNumber TDFrontLeftDriveSpeed;
+  TDNumber TDFrontLeftAngle;
+  TDNumber TDFrontRightDriveSpeed;
+  TDNumber TDFrontRightAngle;
+  TDNumber TDBackLeftDriveSpeed;
+  TDNumber TDBackLeftAngle;
+  TDNumber TDBackRightDriveSpeed;
+  TDNumber TDBackRightAngle;
+
+  TDNumber TDxSpeedMeasured;
+  TDNumber TDySpeedMeasured;
+  TDNumber TDrotSpeedMeasured;
+
+  TDNumber TDPoseX;
+  TDNumber TDPoseY;
+  TDNumber TDPoseAngle;
+  TDNumber TDGyroAngle;
+
+  ChassisSpeeds m_requestSpeeds = new ChassisSpeeds();
+  ChassisSpeeds m_limitSpeeds = new ChassisSpeeds();
+  double m_driveTime = 0;
+
+  TDNumber m_DLeftFrontCurrentOutput;
+  TDNumber m_DRightFrontCurrentOutput;
+  TDNumber m_DLeftBackCurrentOutput;
+  TDNumber m_DRightBackCurrentOutput;
+
+  TDNumber m_TLeftFrontCurrentOutput;
+  TDNumber m_TRightFrontCurrentOutput;
+  TDNumber m_TLeftBackCurrentOutput;
+  TDNumber m_TRightBackCurrentOutput;
 
 
   private Drive() {
@@ -108,7 +164,7 @@ public void addVisionMeasurement(Pose2d pose, double timestamp, Matrix<N3,N1> st
   }
 
   public void drive(ChassisSpeeds speeds) {
-    //ChassisSpeeds limitSpeeds = limitRates(speeds);
+    ChassisSpeeds limitSpeeds = limitRates(speeds);
     var swerveModuleStates = Constants.DriveConstants.kKinematics.toSwerveModuleStates(speeds);
     setModuleStates(swerveModuleStates);
   }
@@ -123,13 +179,47 @@ public void addVisionMeasurement(Pose2d pose, double timestamp, Matrix<N3,N1> st
     drive(chassisSpeeds);
   }
 
-<<<<<<< HEAD
-  
-=======
+  public ChassisSpeeds limitRates(ChassisSpeeds commandedSpeeds) {
+    //Apply Speed Limits
+    double linearSpeed = Math.hypot(commandedSpeeds.vxMetersPerSecond, commandedSpeeds.vyMetersPerSecond);
+    Rotation2d direction = Rotation2d.kZero;
+    if(Math.abs(commandedSpeeds.vxMetersPerSecond) > 1e-6 ||
+       Math.abs(commandedSpeeds.vyMetersPerSecond) > 1e-6){
+      direction = new Rotation2d(commandedSpeeds.vxMetersPerSecond, commandedSpeeds.vyMetersPerSecond);
+    }
+    double limitedSpeed = Math.min(linearSpeed, Constants.DriveConstants.kMaxSpeedMetersPerSecond);
+    double limitedTheta = MathUtil.clamp(commandedSpeeds.omegaRadiansPerSecond, 
+                                            -Constants.DriveConstants.kMaxSpeedMetersPerSecond, 
+                                            Constants.DriveConstants.kMaxAngularSpeed);
+    
+    Translation2d linearVelocity = (new Pose2d(new Translation2d(), direction)
+                                      .transformBy(new Transform2d(new Translation2d(limitedSpeed, 0.0), new Rotation2d()))).getTranslation();
+                        
+    //Apply Acceleration Limits
+    double currentTime = WPIUtilJNI.now() * 1e-6;
+    double accelerationDif = Constants.DriveConstants.kMaxAccelerationMetersPerSecondSquared * (currentTime-m_prevTime);
+    double xSpeed = MathUtil.clamp(linearVelocity.getX(),
+                                  m_lastSpeeds.vxMetersPerSecond - accelerationDif,
+                                  m_lastSpeeds.vxMetersPerSecond + accelerationDif);
+
+    double ySpeed = MathUtil.clamp(linearVelocity.getY(),
+                                 m_lastSpeeds.vyMetersPerSecond - accelerationDif,
+                                 m_lastSpeeds.vyMetersPerSecond + accelerationDif);
+
+    double thetaAccelDif = Constants.DriveConstants.kMaxAngularAcceleration * (currentTime - m_prevTime);
+    double thetaSpeed = MathUtil.clamp(limitedTheta,
+                                 m_lastSpeeds.omegaRadiansPerSecond - thetaAccelDif,
+                                 m_lastSpeeds.omegaRadiansPerSecond + thetaAccelDif);
+
+   ChassisSpeeds limitedSpeeds = new ChassisSpeeds(xSpeed, ySpeed, thetaSpeed);
+   m_lastSpeeds = limitedSpeeds;
+   m_prevTime = currentTime;
+   return limitedSpeeds;
+  }
+
   private void updateTD() {
     m_Field.setRobotPose(getPose());
   }
->>>>>>> e944081693584506339e4c7c4d3966b90bf58b42
 
   @Override
   public void periodic() {
