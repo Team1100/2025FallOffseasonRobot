0870399b4b7100584014fb603a15d7d35b328997
manchuyiabm-byte <manchuyiabm@gmail.com>
2025-10-22 19:28:24 -0400

    Adds V_ENABLED to periodic

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -28,5 +28,6 @@ public class Vision extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    if (RobotMap.V_ENABLED) {}
   }
 }
